3e2fb79e394316c9ce55adae454285b0dfee4cd5
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-08-23 13:18:53 +0200

    [master] spring-boot initlzr setup

---
+++ src/main/java/com/github/huluvu424242/deployview/DeployOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.github.huluvu424242.deployview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployOverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/huluvu424242/deployview/DeployOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.huluvu424242.deployview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
