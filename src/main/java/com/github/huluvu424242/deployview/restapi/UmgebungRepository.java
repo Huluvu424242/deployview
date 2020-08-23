931e4b9990219bb30a7adc091d2e8542f9898c92
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-08-23 21:50:37 +0200

    [feature/gui] UmgebungsRepo disabled

--- src/main/java/com/github/huluvu424242/deployview/restapi/UmgebungRepository.java
+++ src/main/java/com/github/huluvu424242/deployview/restapi/UmgebungRepository.java
@@ -1,11 +1,11 @@
-package com.github.huluvu424242.deployview.restapi;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "umgebungen", path = "umgebungen")
-public interface UmgebungRepository extends PagingAndSortingRepository<Umgebung, Long> {
-
-//    List<Umgebung> findById(@Param("id") String id);
-
-}
+//package com.github.huluvu424242.deployview.restapi;
+//
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//@RepositoryRestResource(collectionResourceRel = "umgebungen", path = "umgebungen")
+//public interface UmgebungRepository extends PagingAndSortingRepository<Umgebung, Long> {
+//
+////    List<Umgebung> findById(@Param("id") String id);
+//
+//}
