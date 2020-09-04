358e0e5731db8c1904104e7f89234e599752f334
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-09-04 22:36:19 +0200

    [simpleJPA] tabellen angelegt

--- src/main/java/com/github/huluvu424242/deployview/restapi/ArtifactRepository.java
+++ src/main/java/com/github/huluvu424242/deployview/restapi/ArtifactRepository.java
@@ -2,7 +2,9 @@ package com.github.huluvu424242.deployview.restapi;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "artifacts", path = "artifacts")
 public interface ArtifactRepository extends CrudRepository<Artifact, Long> {
 
     List<Artifact> findByUmgebung(String umgebung);
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Das Script muss existieren, sonst werden die Tabellen nicht angelegt
+
+-- Das Skript darf nicht leer sein - sonst Exception
+select * from artifact;
